b411760d94727a3f78b289aab3548dbd22fafab3
yetao <2168929065@qq.com>
2021-11-08 15:45:24 +0800

    Error log

--- src/NodeNotePackage/Examples/example.py
+++ src/NodeNotePackage/Examples/example.py
@@ -16,14 +16,24 @@ import os
 
 sys.path.insert(0, os.path.abspath(os.path.join(__file__, "../../../../")))
 
+import traceback
+from datetime import datetime
+
 from src.NodeNotePackage.NodeNote.Components.window import NoteWindow
 from src.NodeNotePackage.NodeNote.GraphicsView.app import TabletApplication
 from PyQt5.QtWidgets import QSplashScreen
 from PyQt5.QtGui import QPixmap, QFont
-from PyQt5.QtCore import Qt
+from PyQt5.QtCore import Qt, qInstallMessageHandler
+
+
+def message_output(msg_type, context, msg):
+    with open("exception_log.txt", "a", encoding="utf-8") as log:
+        log.write(f"{msg} from {context.file}, {context.function}, {context.line}")
 
 
 if __name__ == '__main__':
+    qInstallMessageHandler(message_output)
+
     app = TabletApplication([])
 
     # slash
